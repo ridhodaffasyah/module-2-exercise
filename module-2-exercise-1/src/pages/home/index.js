467fdf49f00961f3cfc9aa10d2a72078ceb899c0
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2022-03-21 14:56:35 +0700

    initialize CRA, add search bar, search button, and gif image

---
+++ module-2-exercise-1/src/pages/home/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaSearch } from "react-icons/fa";
+
+function Index() {
+    return (
+        <>
+            <h1 className='title'>Search Gif<span>.</span></h1>
+                <div className='input'>
+                    <input
+                        placeholder='Cara gambar kesukaanmu'
+                        type='text'
+                        className="search-input"
+                        onFocus={(e) => {e.placeholder=''}}
+                        onBlur={(e) => {e.placeholder='Cara gambar kesukaanmu'}}
+                    >
+                    </input>
+                    <button
+                        value='Search'
+                        type='submit'
+                        className="search-button"
+                    >
+                        <FaSearch />
+                    </button>
+                </div>
+                <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif" className='img' alt="From Giphy"></img>
+        </>
+    );
+}
+
+export default Index;
